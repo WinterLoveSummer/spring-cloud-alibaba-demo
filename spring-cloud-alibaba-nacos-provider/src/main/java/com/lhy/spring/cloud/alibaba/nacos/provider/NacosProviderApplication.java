0ed208aebe41adbd5da652ed5cc7641a62209ef8
WinterLoveSummer <lhy021211@163.com>
2020-09-05 14:47:21 +0800

    测试

---
+++ spring-cloud-alibaba-nacos-provider/src/main/java/com/lhy/spring/cloud/alibaba/nacos/provider/NacosProviderApplication.java
@@ -0,0 +1,13 @@
+package com.lhy.spring.cloud.alibaba.nacos.provider;
+
+/**
+ * @author lhy
+ * @date 2020/9/5 13:24
+ * @desc:
+ */
+
+public class NacosProviderApplication {
+    public static void main(String[] args) {
+
+    }
+}
